c4d7f09ec5482957755c0fe0f446d2db3c7584ef
tokitsu <toki2222son@yahoo.co.jp>
2022-04-29 20:19:30 +0900

    try again! third times

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { setTextRange } from 'typescript';
+import { setTextRange, TodoCommentDescriptor } from 'typescript';
 import './App.css';
 
 function App() {
@@ -9,25 +9,25 @@ function App() {
   type Todo = {
     id: number,
     text: string,
-    checked: boolean
+    checked: boolean,
   }
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // console.log(e.target.value)
+  const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value)
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const changeSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const todo = {
+
+    const newTodo = {
       id: todos.length,
       text: inputText,
       checked: false,
     }
-    setTodos([todo, ...todos]);
+    setTodos([newTodo, ...todos])
   }
 
-  const handleEdit = (value: string, id: number) => {
+  const changeEdit = (id: number, value: string) => {
     const newTodos = todos.map((todo) => {
       if (todo.id === id) {
         todo.text = value
@@ -37,7 +37,12 @@ function App() {
     setTodos(newTodos)
   }
 
-  const handleChecked = (id: number, checked: boolean) => {
+  const onClickDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const changeCheck = (id: number, checked: boolean) => {
     const newTodos = todos.map((todo) => {
       if (todo.id === id) {
         todo.checked = !checked
@@ -47,26 +52,23 @@ function App() {
     setTodos(newTodos)
   }
 
-  const handleDelete = (id: number) => {
-    const newTodos = todos.filter((todo) => todo.id !== id)
-    setTodos(newTodos)
-  }
-
   return (
     <div className="App">
+      <h2>practice_with_ts</h2>
       <div>
-        <h2>todo_with_ts</h2>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input type="text" onChange={(e) => handleInput(e)} />
+        <form onSubmit={(e) => changeSubmit(e)}>
+          <input type="text" onChange={(e) => changeInput(e)} />
           <input type="submit" value="登録" />
         </form>
-        {todos.map((todo) => (
-          <li>
-            <input type="text" value={todo.text} onChange={(e) => handleEdit(e.target.value, todo.id)} disabled={todo.checked} />
-            <input type="checkbox" onChange={() => handleChecked(todo.id, todo.checked)} />
-            <button onClick={() => handleDelete(todo.id)}>削除</button>
-          </li>
-        ))}
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input type="text" value={todo.text} onChange={(e) => changeEdit(todo.id, e.target.value)} disabled={todo.checked} />
+              <input type="checkbox" onClick={() => changeCheck(todo.id, todo.checked)} />
+              <button onClick={() => onClickDelete(todo.id)}>削除</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
